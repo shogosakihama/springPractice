80ecb54b1d827db807a69ef941fd92720a2ea521
Shogo <goshalways@gmail.com>
2024-05-20 23:47:32 +0900

    コントローラー追加

--- spring_boot/server/src/main/java/com/example/app/controller/UserController.java
+++ spring_boot/server/src/main/java/com/example/app/controller/UserController.java
@@ -1,9 +1,14 @@
 package com.example.app.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import com.example.app.repository.UserRepository;
+import com.example.app.entity.User;
+
+import java.util.Optional;
 
 @RestController
 public class UserController {
@@ -19,4 +24,10 @@ public class UserController {
   public String user() {
     return String.valueOf(repository.findAll());
   }
+
+  @GetMapping("/user")
+    public String getUserNameById(@RequestParam Long id) {
+        Optional<User> user = repository.findById(id);
+        return user.map(User::getName).orElse("User not found");
+    }
 }
